ba9ddfa19b74c413ea1d8ba533cb59804d3f9ffa
patricknowlan <patrick@540.co>
2017-11-03 21:48:20 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,75 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Star Wars Characters (DATA)
+// =============================================================
+var users = [
+  {
+    routeName: "yoda",
+    name: "Yoda",
+    role: "Jedi Master",
+    age: 900,
+    forcePoints: 2000
+  },
+  {
+    routeName: "darthmaul",
+    name: "Darth Maul",
+    role: "Sith Lord",
+    age: 200,
+    forcePoints: 1200
+  },
+  {
+    routeName: "obiwankenobi",
+    name: "Obi Wan Kenobi",
+    role: "Jedi Master",
+    age: 55,
+    forcePoints: 1350
+  }
+];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+
+
+// Get all characters
+app.get("/users", function(req, res) {
+  res.json(characters);
+});
+
+// Search for Specific Character (or all characters) - provides JSON
+app.get("/api/:users?", function(req, res) {
+  var chosen = req.params.characters;
+
+  if (chosen) {
+    console.log(chosen);
+
+    for (var i = 0; i < users.length; i++) {
+      if (chosen === users[i].routeName) {
+        return res.json(users[i]);
+      }
+    }
+    return res.json(false);
+  }
+  return res.json(users);
+});
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
