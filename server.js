9654b2a1e14b8c832b01a6b359b9a715ad05acfb
patricknowlan <patrick@540.co>
2017-11-03 23:22:02 -0400

    adding unicoin integration

--- server.js
+++ server.js
@@ -13,7 +13,309 @@ var port = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Star Wars Characters (DATA)
+var Web3 = require('web3');
+web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/"));
+
+var ethInterface = [
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "name",
+    "outputs": [
+      {
+        "name": "",
+        "type": "string",
+        "value": "Unicoin 2.0"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  }, {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_spender",
+        "type": "address"
+      }, {
+        "name": "_value",
+        "type": "uint256"
+      }
+    ],
+    "name": "approve",
+    "outputs": [
+      {
+        "name": "success",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }, {
+    "constant": true,
+    "inputs": [],
+    "name": "totalSupply",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256",
+        "value": "540"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  }, {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_from",
+        "type": "address"
+      }, {
+        "name": "_to",
+        "type": "address"
+      }, {
+        "name": "_value",
+        "type": "uint256"
+      }
+    ],
+    "name": "transferFrom",
+    "outputs": [
+      {
+        "name": "success",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }, {
+    "constant": true,
+    "inputs": [],
+    "name": "decimals",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint8",
+        "value": "0"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  }, {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_value",
+        "type": "uint256"
+      }
+    ],
+    "name": "burn",
+    "outputs": [
+      {
+        "name": "success",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }, {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "balanceOf",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256",
+        "value": "0"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  }, {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "target",
+        "type": "address"
+      }, {
+        "name": "mintedAmount",
+        "type": "uint256"
+      }
+    ],
+    "name": "mintToken",
+    "outputs": [
+      {
+        "name": "success",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }, {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_from",
+        "type": "address"
+      }, {
+        "name": "_value",
+        "type": "uint256"
+      }
+    ],
+    "name": "burnFrom",
+    "outputs": [
+      {
+        "name": "success",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }, {
+    "constant": true,
+    "inputs": [],
+    "name": "symbol",
+    "outputs": [
+      {
+        "name": "",
+        "type": "string",
+        "value": "🦄"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  }, {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_from",
+        "type": "address"
+      }, {
+        "name": "_to",
+        "type": "address"
+      }, {
+        "name": "_value",
+        "type": "uint256"
+      }
+    ],
+    "name": "transfer",
+    "outputs": [
+      {
+        "name": "success",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }, {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "",
+        "type": "address"
+      }, {
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "allowance",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256",
+        "value": "0"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  }, {
+    "inputs": [
+      {
+        "name": "initialSupply",
+        "type": "uint256",
+        "index": 0,
+        "typeShort": "uint",
+        "bits": "256",
+        "displayName": "initial Supply",
+        "template": "elements_input_uint",
+        "value": "540"
+      }, {
+        "name": "tokenName",
+        "type": "string",
+        "index": 1,
+        "typeShort": "string",
+        "bits": "",
+        "displayName": "token Name",
+        "template": "elements_input_string",
+        "value": "Unicoin 2.0"
+      }, {
+        "name": "tokenSymbol",
+        "type": "string",
+        "index": 2,
+        "typeShort": "string",
+        "bits": "",
+        "displayName": "token Symbol",
+        "template": "elements_input_string",
+        "value": "🦄"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  }, {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "name": "from",
+        "type": "address"
+      }, {
+        "indexed": true,
+        "name": "to",
+        "type": "address"
+      }, {
+        "indexed": false,
+        "name": "value",
+        "type": "uint256"
+      }
+    ],
+    "name": "Transfer",
+    "type": "event"
+  }, {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "name": "from",
+        "type": "address"
+      }, {
+        "indexed": false,
+        "name": "value",
+        "type": "uint256"
+      }
+    ],
+    "name": "Burn",
+    "type": "event"
+  }
+];
+
+var myContract = new web3.eth.Contract(ethInterface, "0x9b143D56cAf24e471BB54a50F159401F420FfAAe", {from: '0x75f628f2aCB0ADa5A48D329319c185dbf7f2BB59'});
+
 // =============================================================
 var users = [
   {
@@ -22,7 +324,7 @@ var users = [
     "firstName": "Jeremy",
     "lastName": "Lundstrom",
     "walletAddress": "0x0e9b86F67E32898A6080D595616486FeFb0b46BD",
-    "unicoinBalance": null,
+    "unicoinBalance": 0,
     "transactions": [],
     "picture": "picture.png",
     "jobTitle": "Consultant"
@@ -32,29 +334,29 @@ var users = [
     "userName": "kirkpatrick",
     "firstName": "Cullen",
     "lastName": "Kirkpatrick",
-    "walletAddress": "0x0e9b86F67E32898A6080D595616486FeFb0b46BD",
-    "unicoinBalance": null,
-    "transactions": [],
-    "picture": "picture.png",
-    "jobTitle": "Consultant"
-  },
-  {
-    "id": 1,
-    "userName": "knowlan",
-    "firstName": "Patrick",
-    "lastName": "Knowlan",
-    "walletAddress": "0x022e7A06f2DE6FD453227fEfa29fc5FD490879B5",
-    "unicoinBalance": null,
+    "walletAddress": "0x75f628f2aCB0ADa5A48D329319c185dbf7f2BB59",
+    "unicoinBalance": 0,
     "transactions": [],
     "picture": "picture.png",
     "jobTitle": "Consultant"
   },
   {
     "id": 3,
+    "userName": "knowlan",
+    "firstName": "Patrick",
+    "lastName": "Knowlan",
+    "walletAddress": "0x022e7A06f2DE6FD453227fEfa29fc5FD490879B5",
+    "unicoinBalance": 0,
+    "transactions": [],
+    "picture": "picture.png",
+    "jobTitle": "Consultant"
+  },
+  {
+    "id": 4,
     "userName": "sharpe",
     "firstName": "Larry",
     "lastName": "Sharpe",
-    "walletAddress": "0x0e9b86F67E32898A6080D595616486FeFb0b46BD",
+    "walletAddress": "0x5f5c03f9bdb02e911ca84d0c350da895a5769137",
     "unicoinBalance": 0,
     "transactions": [],
     "picture": "picture.png",
@@ -77,19 +379,19 @@ app.get("/users", function(req, res) {
 // Search for Specific Character (or all characters) - provides JSON
 app.get("/users/:id?", function(req, res) {
   var chosen = req.params.id;
-  console.log(chosen);
+  var selectedUser;
 
   if (chosen) {
-    console.log(chosen);
-
     for (var i = 0; i < users.length; i++) {
       if (chosen == users[i].id) {
-        return res.json(users[i]);
+        selectedUser = users[i];
+        myContract.methods.balanceOf(users[i].walletAddress).call({from: '0x75f628f2aCB0ADa5A48D329319c185dbf7f2BB59'}, function(error, result){
+          selectedUser.unicoinBalance = result;
+          return res.json(selectedUser);
+        });
       }
     }
-    return res.json(false);
   }
-  return res.json(users);
 });
 
 
